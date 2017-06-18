badfadae4416ac4f1e5067639ae92b2b13173eeb
tutuman <praman.hamal@yahoo.com>
2017-06-18 17:53:23 -0500

    first
    First

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//Connect to Database
+mongoose.connect(config.database);
+
+//On Connection
+mongoose.connection.on('connected', ()=>{
+	console.log('Connected to database ' + config.database);
+});
+
+//On Error
+mongoose.connection.on('error', (err)=>{
+	console.log('Database error: ' + err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+const port = 3000;
+
+//Cors middleware
+app.use(cors());
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+
+app.use('/users', users);
+
+app.get('/', (req, res)=>{
+	res.send('Invalid end Point');
+});
+
+app.listen(port, ()=>{
+	console.log('Server started on port ' + port);
+});
---
+++ models/user.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+//User Schema
+const UserSchema = mongoose.Schema({
+	name: {
+		type: String
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	username: {
+		type: String,
+		required: true
+	},
+	password:{
+		type: String,
+		required: true
+	},
+	lat:{
+		type: Number,
+		required: false
+	},
+	lng:{
+		type: Number,
+		required: false
+	},
+	rating:{
+		type: Number,
+		required: false
+	}
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+	const query = {username: username}
+	User.findOne(query, callback);
+}
+
+module.exports.addUser = function(newUser, callback){
+	bcrypt.genSalt(10, (err, salt) => {
+		bcrypt.hash(newUser.password, salt, (err, hash) =>{
+			if(err) throw err;
+			newUser.password = hash;
+			newUser.save(callback);
+		});
+	});
+}
+
+
+
+module.exports.comparePassword = function(candidataPassword, hash, callback){
+	bcrypt.compare(candidataPassword, hash, (err, isMatch)=>{
+		if(err) throw err;
+		callback(null, isMatch);
+	});
+}
